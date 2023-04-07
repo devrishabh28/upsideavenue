cc0fe1c8ddfd09231ea57012e043a3d8fbb5302e
Rishabh Dev <devrishabh28@gmail.com>
2023-04-07 16:41:00 +0530

    added photo upload support for add properties

--- src/main/java/com/dbmsproject/upsideavenue/controllers/UserController.java
+++ src/main/java/com/dbmsproject/upsideavenue/controllers/UserController.java
@@ -1,5 +1,12 @@
 package com.dbmsproject.upsideavenue.controllers;
 
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,9 +15,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.dbmsproject.upsideavenue.models.Photo;
 import com.dbmsproject.upsideavenue.models.Property;
 import com.dbmsproject.upsideavenue.models.User;
+import com.dbmsproject.upsideavenue.repositories.PhotoRepository;
 import com.dbmsproject.upsideavenue.repositories.PropertyRepository;
 
 @Controller
@@ -20,6 +31,9 @@ public class UserController {
 
     @Autowired
     private PropertyRepository propertyRepository;
+
+    @Autowired
+    private PhotoRepository photoRepository;
 
     @GetMapping("properties")
     public String properties() {
@@ -33,13 +47,23 @@ public class UserController {
     }
 
     @PostMapping("properties/add")
-    public String addProperty(Property property) {
+    public String addProperty(Property property, @RequestParam("images") MultipartFile[] images)
+            throws SerialException, SQLException, IOException {
 
         User owner = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         property.setOwner(owner);
 
         propertyRepository.save(property);
+
+        for (MultipartFile image : images) {
+            Photo photo = new Photo();
+            photo.setPropertyId(property);
+            Blob imageBlob = new SerialBlob(image.getBytes());
+            photo.setPhoto(imageBlob);
+
+            photoRepository.save(photo);
+        }
 
         return "properties";
     }
